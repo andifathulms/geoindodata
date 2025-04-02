cbec1430e2ae0d84cce35a56a652382d8ed29d7b
Andi Fathul Mukminin <officialandifathul@gmail.com>
2025-04-03 01:06:24 +0700

    province index

--- geoindodata/regions/urls.py
+++ geoindodata/regions/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 app_name = "regions"
 
 urlpatterns = [
-    path('geoghrapic-units/', include("geoindodata.regions.geographic_units.urls"))
+    path('geoghrapic-units/', include("geoindodata.regions.geographic_units.urls")),
+    path('provinces/', include("geoindodata.regions.provinces.urls"))
 ]
