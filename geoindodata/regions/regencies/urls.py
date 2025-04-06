721a2e2a1f389c662298bb6e6ac807f877e8e4b8
Andi Fathul Mukminin <officialandifathul@gmail.com>
2025-04-06 22:53:52 +0700

    regency details

--- geoindodata/regions/regencies/urls.py
+++ geoindodata/regions/regencies/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = "regencies"
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:pk>/', views.details, name="details")
 ]
--- geoindodata/regions/regencies/views.py
+++ geoindodata/regions/regencies/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from geoindodata.apps.regions.models import Regency
 
@@ -19,3 +19,9 @@ def index(request: HttpRequest) -> HttpResponse:
         'is_paginated': page_obj.has_other_pages(),
     }
     return render(request, 'regions/regencies/index.html', context)
+
+
+def details(request: HttpRequest, pk: int) -> HttpResponse:
+    regency = get_object_or_404(Regency.objects.select_related('province'), pk=pk)
+
+    return render(request, 'regions/regencies/details.html', {'regency': regency})
